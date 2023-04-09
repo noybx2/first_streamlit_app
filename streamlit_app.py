8883a9e381a04d215944b1143ee24f22ed989aac
noybx2 <115945512+noybx2@users.noreply.github.com>
2023-04-09 18:10:03 +0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,4 @@
-import streamlit 
-streamlit.title("title")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

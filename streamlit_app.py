5ecd30509a6a9077cab5ae770db80195d11b3ba1
noybx2 <115945512+noybx2@users.noreply.github.com>
2023-04-10 16:24:34 +0300

    com12

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user added ', add_my_fruit)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+add_my_fruit)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

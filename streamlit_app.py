8d55892000974fed46acc86f61125ca9ba5e2390
noybx2 <115945512+noybx2@users.noreply.github.com>
2023-04-10 16:46:13 +0300

    com15

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+    
 try:
     # streamlit.text(fruityvice_response.json())
     # write your own comment -what does the next line do? 
@@ -31,10 +37,8 @@ try:
     if not fruit_choice:
         streamlit.error("pls select a fruit to get information")
     else:
-        streamlit.write('The user entered ', fruit_choice)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except:
     streamlit.error()
 streamlit.stop()

e95af7921c3f47b5eb991fb19bff58507ba10fc6
noybx2 <115945512+noybx2@users.noreply.github.com>
2023-04-10 17:06:55 +0300

    com16

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,20 +41,34 @@ try:
         streamlit.dataframe(back_from_function)
 except:
     streamlit.error()
+
+streamlit.header("the fruit load list contains: ")
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+if stramlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')")
+        return "thanks for adding "+new_fruit
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Banana')
-streamlit.write('The user added ', add_my_fruit)
-fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/"+add_my_fruit)
-fruityvice_normalized2 = pandas.json_normalize(fruityvice_response2.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized2)
-streamlit.write("Thanks for adding ", add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if stramlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+# streamlit.write('The user added ', add_my_fruit)
+# fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/"+add_my_fruit)
+# fruityvice_normalized2 = pandas.json_normalize(fruityvice_response2.json())
+# # write your own comment - what does this do?
+# streamlit.dataframe(fruityvice_normalized2)
+# streamlit.write("Thanks for adding ", add_my_fruit)
